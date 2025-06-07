251e794cd68f200e5cf2a0a4091a6b6bcd9702e4
Maite Perez Vento <maitepv87@gmail.com>
2025-06-06 23:13:51 -0400

    Rename

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   const [place, setPlace] = useState<Place | null>(null);
 
   return (
-    // <div className="m-4h- screen w-screen grid grid-cols-12">
     <div className="h-screen w-screen grid grid-cols-12">
       <div className="col-span-3 p-2">
         <LocationSearch onPlaceClick={(p) => setPlace(p)} />
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,34 +2,10 @@ import "leaflet/dist/leaflet.css";
 import type { Place } from "../api/Place";
 import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
-// import type { Map as LeafletMap } from "leaflet";
 
 interface MapProps {
   place: Place | null;
 }
-
-// export const Map = ({ place }: MapProps) => {
-//   const mapRef = useRef<LeafletMap | null>(null);
-
-//   useEffect(() => {
-//     if (mapRef.current && place) {
-//       mapRef.current.flyTo([place.latitude, place.longitude]);
-//     }
-//   }, [place]);
-
-//   return (
-//     <MapContainer
-//       ref={mapRef}
-//       center={[40.7, -74]}
-//       zoom={12}
-//       scrollWheelZoom
-//       className="h-full"
-//     >
-//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-//       {place && <Marker position={[place.latitude, place.longitude]} />}
-//     </MapContainer>
-//   );
-// };
 
 const MapComponent = ({ place }: MapProps) => {
   const map = useMap();
